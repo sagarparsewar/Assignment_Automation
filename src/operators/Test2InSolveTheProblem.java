271f22c0f693accd686d6316d12e9bbfaa53266c
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-04-20 16:15:13 +0530

    this program output is the 1

---
+++ src/operators/Test2InSolveTheProblem.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class Test2InSolveTheProblem {
+         private static int x;
+         public static void main(String[] args) {
+			System.out.println(fun());
+			
+		}
+    static int fun() {
+			// TODO Auto-generated method stub
+			return ++x;
+		}
+static int from () {
+	   return++x;
+   }
+}
