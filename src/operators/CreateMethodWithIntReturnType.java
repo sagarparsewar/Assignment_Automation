79d2b446b85afe7707939ee9ab8686e2ab51038c
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-04-24 22:02:13 +0530

    create 2 methods one should be static and one non static method

---
+++ src/operators/CreateMethodWithIntReturnType.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class CreateMethodWithIntReturnType {
+	//non static method
+	public int add1(int i, int j) {
+		int k = i+j;
+		return k;
+	}
+	// static method
+	public static int add (int i, int j) {
+		int k2 =i + j;
+		return k2;
+
+	}
+}
