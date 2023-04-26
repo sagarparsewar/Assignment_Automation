c68e7d59a39f12164d560f9bd6ccaa2ac4f5c288
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-04-26 17:21:20 +0530

    Write static method and check visibility with private, default, protected and public

--- src/operators/Test_3_staicMethod.java
+++ src/operators/Test_3_staicMethod.java
@@ -1,5 +1,27 @@
 package operators;
 
 public class Test_3_staicMethod {
+	//private :
+	class Test_1{
+		private static void privatemethod() {
 
+		}
+	}
+	//Default
+	class Test_2{
+		static void defaultmethod() {
+
+		}
+	}
+	//protected
+	public class Test_3{
+		protected static void protectedMethod() {
+		}
+	}
+	//public 
+	public class Test {
+		public static void publicMethod() {
+
+		}
+	}
 }
