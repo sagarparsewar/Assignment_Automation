071fb4603c67eb3a79d7d142191d0019949ccafc
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-04-27 14:59:55 +0530

    What class cannot have access modifier as private and protracted

---
+++ src/operators/ClassnotAccessModifier.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class ClassnotAccessModifier {
+	public class myclass{
+		//method here
+	}
+
+	class myclass1{
+		//method here
+	}
+
+	private class myclass2{
+		//class and method here
+	}
+	protected class myclass3{
+		//class and method here
+	}
+
+
+}
