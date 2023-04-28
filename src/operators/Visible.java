b14dc0228a0aacf081165fd579fb9c21198e57e7
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-04-28 14:51:26 +0530

    Write static variable and check visibility with private, default, protected and public

---
+++ src/operators/Visible.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class Visible {
+   private static int privateStaticVariable;
+   // getter and setter method 
+}
+class Myclass{
+	static int defaultStaticVariable;
+}
+class Myclass1{
+	protected static int protectedStaticvariable;
+	
+}
+class Myclass2{
+	public static int publicStaticVariable;
+}
