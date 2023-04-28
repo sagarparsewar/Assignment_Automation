035dcd7398acda348f6ccb0958a6b77785ae87f7
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-04-28 15:30:25 +0530

    Write Output of below program= 00

---
+++ src/oops/Test.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Test {
+ protected int x,y;
+ 
+	 
+ }
+
+class Main{
+	public static void main(String[] args) {
+		Test t = new Test();
+		System.out.println(t.x+""+t.y);
+	}
+}
