3794eafb47db2553d0d5e10bf74955154a6d1a02
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-05-19 11:25:05 +0530

    default memory location for string in java

---
+++ src/oops/DefultString.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class DefultString {
+	public static void main(String[] args) {
+		String str = "Hello,world!";
+		int hashcode = str.hashCode();
+		System.out.println("DefultString memory location(hashcode):"+ hashcode);
+	}
+
+}
