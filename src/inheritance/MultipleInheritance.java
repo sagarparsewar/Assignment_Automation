85ab3940b48886821d873553870be8aa4c260bd3
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-05-25 12:13:29 +0530

    single Inheritance multi level Inheritance Multiple Inheritance Hierachical Inheritance

---
+++ src/inheritance/MultipleInheritance.java
@@ -0,0 +1,51 @@
+package inheritance;
+
+public class MultipleInheritance {
+
+}
+//single level Inheritance 
+class Superclass{
+	
+}
+class Subclass extends Superclass{
+	
+}
+// Multilevel Inheritance
+
+
+class Superclass1{
+	
+}
+class IntermediateClass extends Superclass1{
+	
+}
+class Subclass1 extends IntermediateClass{
+	
+}
+
+
+
+//Multiple Inheritance 
+
+class SuperclassA{
+	
+}
+class SuperclassB{
+	
+}
+class SubclassC extends SuperclassA{
+	
+}
+
+
+
+//Hierachical inheritance
+class Superclass5{
+	
+}
+class Subclass2 extends Superclass{
+	
+}
+class Subclass6 extends Superclass5{
+	
+}
