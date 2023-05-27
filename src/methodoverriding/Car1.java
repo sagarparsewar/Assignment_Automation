780d57b76fbbbe13f93a72b3408e18d433c40b01
SAGAR <SAGAR@DESKTOP-S6MMN7F>
2023-05-27 17:53:51 +0530

    Encapsulation and abstraction

---
+++ src/methodoverriding/Car1.java
@@ -0,0 +1,25 @@
+package methodoverriding;
+ 
+
+//Encapsulation
+public class Car1 {
+private String Model;
+private int year;
+private String model;
+public String getModel() {
+	return getModel();
+}
+public void setModel(String model) {
+	this.model = model;
+	}
+public int getYear() {
+	return year;
+}
+public void setYear(int year) {
+	this.year = year;
+}
+//Abstract 
+public abstract class shape{
+	public abstract double getArea();
+}
+}
